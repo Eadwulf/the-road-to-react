6e9bed128e7383cf1d6eedf79775b12feb4eab43
Eadwulf <eadwulfts@protonmail.com>
2021-09-14 02:40:10 -0400

    react props

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
 import './App.css';
 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      <h1>The Road To React</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-
-      <hr />
-
-      <List />
-
-    </div>
-  );
-}
-
-function List() {
-  return list.map(function(item) {
+function List(props) {
+  return props.list.map(item => {
       return (
         <div key={item.objectID}>
           <span><a href={item.url}>{item.title}</a></span>
@@ -47,6 +12,41 @@ function List() {
         </div>
       );
   });
+}
+
+function App() {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>The Road To React</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <hr />
+      <List list={stories} />
+    </div>
+  );
 }
 
 export default App;
